3c7aa1da55a903beb2d3a3434ff322edf96c300b
Sid Ahrend <sidahrend57@gmail.com>
2019-10-25 16:48:27 -0600

    fixed provider issue

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { withRouter } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import Routes from "./routes";
 import { Provider } from "react-redux";
+import store from './redux/store';
 
 class App extends Component {
   render() {
     return (
-      // <Provider>
+      <Provider store={store}>
         <div className="App">
           {this.props.location.pathname == "/" ? null : <Nav />}
           <Routes />
         </div>
-      // {/* </Provider> */}
+      </Provider>
     );
   }
 }
